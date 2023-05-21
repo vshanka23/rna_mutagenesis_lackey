74cdfd929ad99c7ee40405c9f8f32b19d2ab44d8
vshanka23 <vshanka@clemson.edu>
2023-05-21 10:07:49 -0400

    Made 6_align_process.sh scale with threads and threadstb so that mafft can use more to speed up alignment. Made corresponding change in Snakefile_degen. Set to max 40 number, users will need to change this on the fly based on their needs and resource availability

--- library/6_align_process.sh
+++ library/6_align_process.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 # Check if the correct number of arguments were passed
-if [ "$#" -ne 2 ]; then
-    echo "ERROR: Missing input parameters. Usage: $0 input_file output_file"
+if [ "$#" -ne 4 ]; then
+    echo "ERROR: Missing input parameters. Usage: $0 input_file output_file threads threadtb"
     exit 1
 fi
+
+# Assign input parameters to variables
+input_file="$1"
+output_file="$2"
+threads="$3"
+threadtb="$4"
 
 # Add mafft module
 module add mafft
 
 # Align the concatenated sequences using MAFFT
-mafft --op 10 --ep 5 --thread 8 --threadtb 5 --threadit 0 --reorder --anysymbol --large --retree 2 "${1}" > "${2}_aligned.fasta"
+mafft --op 10 --ep 5 --thread "$threads" --threadtb "$threadtb" --threadit 0 --reorder --anysymbol --large --retree 2 "$input_file" > "${output_file}_aligned.fasta"
 
 # Get alignment output from MAFFT and linearize it again
-awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < "${2}_aligned.fasta" > "${2}_aligned_linear.fasta"
+awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < "${output_file}_aligned.fasta" > "${output_file}_aligned_linear.fasta"
 
 # Remove sequence IDs from aligned file
-sed '/^>/d' "${2}_aligned_linear.fasta" > "${2}"
+sed '/^>/d' "${output_file}_aligned_linear.fasta" > "$output_file"
 
 # Remove intermediate files
-rm "${2}_aligned.fasta" "${2}_aligned_linear.fasta"
+rm "${output_file}_aligned.fasta" "${output_file}_aligned_linear.fasta"
 
