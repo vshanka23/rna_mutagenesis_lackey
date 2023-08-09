d57a07f61ce14a1fb27c19cc0e9762b3a8fd22fa
vshanka23 <vshanka@clemson.edu>
2023-08-09 14:33:32 -0400

    lots of different changes (reduced the key target to account for low quality, fixed a bug in 10 where all of the file lines were included in grep leading to significant waste of time, created a new 9 to deal with 32bit memory limitation of Usearch - lines are now no longer required, etc)

--- library/10_fasta_to_fastq.sh
+++ library/10_fasta_to_fastq.sh
@@ -12,20 +12,26 @@ fastq_file="$2"
 output_file="$3"
 
 # Create a temporary file to store the sequences in FASTA format
-tmp_fasta=$(mktemp)
+#tmp_fasta=$(mktemp)
 
 # Convert FASTA sequences to single-line format (if not already in single line)
-awk '!/^>/{ printf "%s", $0; n = "\n" } /^>/{ print n $0; n = "" } END{ printf "%s", n }' "$fasta_file" > "$tmp_fasta"
+#awk '!/^>/{ printf "%s", $0; n = "\n" } /^>/{ print n $0; n = "" } END{ printf "%s", n }' "$fasta_file" > "$tmp_fasta"
 
 # Create an empty output file
 > "$output_file"
 
 # Read the FASTA sequences one by one
+#while IFS= read -r sequence
+#do
+  # Extract the matching sequence from the FASTQ file
+#  grep -B 1 -A 2 -F -m 1 -w "$sequence" "$fastq_file" >> "$output_file"
+#done < "$tmp_fasta"
+
 while IFS= read -r sequence
 do
   # Extract the matching sequence from the FASTQ file
   grep -B 1 -A 2 -F -m 1 -w "$sequence" "$fastq_file" >> "$output_file"
-done < "$tmp_fasta"
+done < <(awk '/^[^>]/ { printf "%s", $0; n = "\n" } END { printf "%s", n }' "$fasta_file")
 
 # Clean up the temporary file
 rm "$tmp_fasta"
---
+++ library/9.6_mk_degen_library.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $# -ne 6 ]]; then
+  echo "Usage: $0 <alignment_output_txt> <degen_output_file> <output_dedup_fasta> <output_dedup_linear_fasta> <degen_uniq_txt> <library_dir>"
+  exit 1
+fi
+
+# input filenames
+alignment_output_txt="$1"
+degen_output_file="$2"
+
+# output filenames
+output_dedup_fasta="$3"
+output_dedup_linear_fasta="$4"
+degen_uniq_txt="$5"
+degen_library_dir="$6"
+
+output_dedup_fasta="$1"
+output_dedup_linear_fasta="$1"
+
+# Removing perfect duplicates (in sequence, not in indices. There are 78 duplicates in indices)
+#/data/software/usearch11.0.667_i86linux32 --cluster_fast "$alignment_output_txt" -id 1.00 -centroids "$output_dedup_fasta" -uc "${output_dedup_fasta}_cluster.nc"
+
+# Linearize the final output
+#awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < "$output_dedup_fasta" > "$output_dedup_linear_fasta"
+
+# Find unique barcodes
+cat "$degen_output_file" | sort | uniq > "$degen_uniq_txt"
+
+# Create a library based on barcodes
+mkdir -p "$degen_library_dir"
+cat "$degen_uniq_txt" | while read line; do search_string=">${line}_"; grep -A 1 --no-group-separator ${search_string} "$output_dedup_linear_fasta" > "${degen_library_dir}/${line}.fasta"; grep -oP "${search_string}\K.*" "$output_dedup_linear_fasta" > "${degen_library_dir}/${line}.bc"; done
